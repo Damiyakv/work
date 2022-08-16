c7b2469a9c8ab667e616c7a85be66de618e2522e
Damiyakv <damiyakv@umich.edu>
2022-08-16 20:27:50 +0530

    initial portfolio

---
+++ email.php
@@ -0,0 +1,26 @@
+
+<?php
+// Get data from form 
+$name = $_POST['name'];
+$email= $_POST['email'];
+$message= $_POST['message'];
+ 
+$to = "kvdamiya@gmail.com";
+$subject = "Message Contact form";
+ 
+// The following text will be sent
+// Name = user entered name
+// Email = user entered email
+// Message = user entered message
+$txt ="Name = ". $name . "\r\n  Email = "
+    . $email . "\r\n Message =" . $message;
+ 
+$headers = "From: noreply@demosite.com" . "\r\n" .
+            "CC: somebodyelse@example.com";
+if($email != NULL) {
+    mail($to, $subject, $txt, $headers);
+}
+ 
+// Redirect to
+footer("Location:index.html");
+?>
